2d84859b7b4daa8dd32def42727e94e746b85e3e
Sander Knopper <sander@knopper-ict.nl>
2022-12-30 16:42:38 +0100

    Day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,13 +1,38 @@
-use pathfinding::prelude::{astar, Matrix};
+use pathfinding::prelude::{bfs, Matrix};
+
 fn main() {
     println!("Day 12");
 
-    let input = include_str!("../example/part1.txt");
-    let hill = Matrix::from_rows(input.lines().map(str::bytes)).unwrap();
+    let input = include_str!("../input/part1.txt");
+
+    let mut hill = Matrix::from_rows(input.lines().map(str::bytes)).unwrap();
     let start = hill.keys().find(|p| hill[*p] == b'S').unwrap();
     let end = hill.keys().find(|p| hill[*p] == b'E').unwrap();
+    hill[start] = b'a';
+    hill[end] = b'z';
+    let hill = &hill;
 
-    println!("{:?}", hill);
-    println!("start: {:?}", start);
-    println!("end: {:?}", end);
+    let steps_part1 = bfs(
+        &start,
+        |&current_point| {
+            hill.neighbours(current_point, false).filter(move |&candidate| hill[candidate] <= hill[current_point] + 1)
+        },
+        |&p| p == end,
+    )
+    .unwrap()
+    .len() - 1;
+
+    println!("part 1: {}", steps_part1);
+
+    let steps_part2 = bfs(
+        &end,
+        |&current_point| {
+            hill.neighbours(current_point, false).filter(move |&candidate| hill[current_point] <= hill[candidate] + 1)
+        },
+        |&p| hill[p] == b'a',
+    )
+    .unwrap()
+    .len() - 1;
+
+    println!("part 2: {}", steps_part2);
 }
